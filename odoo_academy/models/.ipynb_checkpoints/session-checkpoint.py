079e1bb6d7873756e62ace35f95920ecb6f01be2
celso-iagrinet <cinzunza@iagrinet.com>
2022-09-28 14:39:57 +0000

    Agregar Modelo Sesion, con menus y vistas, se agregaron cambios many2many, many2one

---
+++ odoo_academy/models/.ipynb_checkpoints/session-checkpoint.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Session(models.Model):
+    _name='academy.session'
+    _description = 'Session info'
+    
+    course_id = fields.Many2one(comodel_name='academy.course',
+                               string='Course',
+                               ondelete='cascade',
+                               required=True)
+    name = fields.Char(string='Title', related='course_id.name')
+    
+    #model que tiene los contactos de odoo
+    instructor_id = fields.Many2one(comodel_name='res.partner', string='Instructor')
+    
+    student_ids = fields.Many2many(comodel_name='res.partner', string='Students')
+    
